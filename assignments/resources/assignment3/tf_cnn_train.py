6951b77eec93fbe867f9b23649c35d0f509f01b2
w4zir <mmk.wazir@gmail.com>
2017-05-31 12:45:43 +0500

    assignment 3 edit

---
+++ assignments/resources/assignment3/tf_cnn_train.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+""" Convolutional network applied to CIFAR-10 dataset classification task.
+References:
+    Learning Multiple Layers of Features from Tiny Images, A. Krizhevsky, 2009.
+Links:
+    [CIFAR-10 Dataset](https://www.cs.toronto.edu/~kriz/cifar.html)
+
+    CSAL4243 Introduction to Machine Learning's assignment 3.
+"""
+from __future__ import division, print_function, absolute_import
+
+import tflearn
+from tflearn.data_utils import shuffle, to_categorical
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
+
+# Data loading and preprocessing
+from tflearn.datasets import cifar10
+(X, Y), (X_test, Y_test) = cifar10.load_data()
+X, Y = shuffle(X, Y)
+Y = to_categorical(Y, 10)
+Y_test = to_categorical(Y_test, 10)
+
+# Real-time data preprocessing
+img_prep = ImagePreprocessing()
+img_prep.add_featurewise_zero_center()
+img_prep.add_featurewise_stdnorm()
+
+# Real-time data augmentation
+img_aug = ImageAugmentation()
+img_aug.add_random_flip_leftright()
+img_aug.add_random_rotation(max_angle=25.)
+
+# Convolutional network building
+## To Do.
+    ## Define your network here
+
+# Train using classifier
+## To Do
+    ## Define model and assign network
+    ## Call the fit function for training
+
+# Manually save model
+## To Do
+    ## Save model
